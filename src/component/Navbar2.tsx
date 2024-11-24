090b25fa91c5ddcf570695ca700d8ae455cf0915
shailjayadav30 <shailjayadav7275@gmail.com>
2024-11-24 11:50:52 +0530

    fifth

--- src/component/Navbar2.tsx
+++ src/component/Navbar2.tsx
@@ -1,35 +1,3 @@
-// import { NavLink } from 'react-router-dom'
-
-// const Navbar2 = () => {
-//   return (
-//     <div className="p-4 bg-white flex items-center justify-center">
-//     <nav className="flex justify-between items-center">
-//       <ul className="flex gap-6 text-lg font-semibold">
-//         {["Renters", "Landlords/Property Managers", "Agents"].map((city) => (
-//           <li key={city}>
-//             <NavLink 
-//               to={`/${city.toLowerCase()}`} 
-//               className={({ isActive }) =>
-//                 `px-4 py-2 rounded-full hover:text-[#0D28AB] transition duration-200 ${
-//                   isActive ? "outline outline-2 outline-[#0D28AB] text-[#0D28AB]" : ""
-//                 }`
-//               }
-//             >
-//               {city}
-//             </NavLink>
-//           </li>
-//         ))}
-//       </ul>
-
-     
-//     </nav>
-//   </div>
-//   )
-// }
-
-// export default Navbar2
-
-
 
 
 import { NavLink } from 'react-router-dom'
--- src/component/Navbar3.tsx
+++ src/component/Navbar3.tsx
@@ -1,33 +1,3 @@
-// import { NavLink } from 'react-router-dom'
-
-// const Navbar3 = () => {
-//   return (
-//     <div className="p-4 bg-gray-100 flex items-center justify-center">
-//     <nav className="flex justify-between items-center">
-//       <ul className="flex gap-6 text-lg font-semibold">
-//         {["Popular", "Investment Hotspots", "Affordable","Great Lifestyle"].map((city) => (
-//           <li key={city}>
-//             <NavLink 
-//               to={`/${city.toLowerCase()}`} 
-//               className={({ isActive }) =>
-//                 `px-4 py-2 rounded-full hover:text-[#0D28AB] transition duration-200 ${
-//                   isActive ? "outline outline-2 outline-[#0D28AB] text-[#0D28AB]" : ""
-//                 }`
-//               }
-//             >
-//               {city}
-//             </NavLink>
-//           </li>
-//         ))}
-//       </ul>
-
-     
-//     </nav>
-//   </div>
-//   )
-// }
-
-// export default Navbar3
 
 
 
